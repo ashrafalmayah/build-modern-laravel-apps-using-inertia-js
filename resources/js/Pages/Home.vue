2ad05e093e31b01ecac14ab744bc10092f880895
ashrafalmayah <ashrafalmayah00@gmail.com>
2024-05-08 15:17:02 +0300

    Pages

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,13 @@
+<script setup>
+defineProps({
+    name: String,
+    frameworks: Array,
+})
+</script>
+<template>
+<h1>Hello {{ name }}</h1>
+<h3>The frameworks we will use: </h3>
+<ul>
+    <li v-for="framework in frameworks">{{ framework }}</li>
+</ul>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return inertia('Home', [
+        'name' => "Ashraf Almayah",
+        'frameworks' => [
+            'Laravel',
+            'Inertia',
+            'Vue'
+        ]
+    ]);
 });
