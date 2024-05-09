2b04e0a5662cf5aec20aafc24f6f3120c78e2d01
ashrafalmayah <ashrafalmayah00@gmail.com>
2024-05-09 14:40:40 +0300

    shared data

--- resources/js/shared/Layout.vue
+++ resources/js/shared/Layout.vue
@@ -1,10 +1,17 @@
 <script setup>
+import { usePage } from "@inertiajs/vue3";
 import Nav from "./Nav.vue";
+import { computed } from "vue";
+
+let username = computed(() => usePage().props.auth.user.username);
 </script>
 
 <template>
     <header class="bg-gray-200 p-4 flex space-x-6 items-center justify-center">
-        <h1 class="font-bold text-xl absolute left-6">My App</h1>
+        <div class="absolute left-6 flex items-center space-x-2">
+            <h1 class="font-bold text-xl">My App</h1>
+            <p class="text-lg">Welcome Back, {{ username }}!</p>
+        </div>
 
         <Nav />
     </header>
