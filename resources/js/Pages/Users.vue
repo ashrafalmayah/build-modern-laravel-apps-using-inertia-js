c7b8946a43a8ff439c32383efd323da630c7404c
ashrafalmayah <ashrafalmayah00@gmail.com>
2024-05-09 11:40:39 +0300

    preserve scrolling

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -1,8 +1,19 @@
 <script setup>
+import { Link } from "@inertiajs/vue3";
 import Nav from "../shared/Nav.vue";
+
+defineProps({
+    time: String
+})
 </script>
 <template>
     <h1>Users</h1>
 
     <Nav />
+
+    <div class="mt-[700px]">
+        <p>The time is {{ time }}.</p>
+
+        <Link class="text-blue-600 hover-underline" preserve-scroll href="/users">Refresh</Link>
+    </div>
 </template>
