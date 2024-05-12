ee6a85cde70db738686dc7768903e44f004d4472
ashrafalmayah <ashrafalmayah00@gmail.com>
2024-05-12 13:06:24 +0300

    inertias form helper

--- resources/js/Pages/Users/Create.vue
+++ resources/js/Pages/Users/Create.vue
@@ -1,18 +1,13 @@
 <script setup>
-import { router } from "@inertiajs/vue3";
-import { reactive } from "vue";
+import { useForm } from "@inertiajs/vue3";
 
-defineProps({
-    errors: Object,
-});
-
-let form = reactive({
+let form = useForm({
     name: "",
     email: "",
     password: "",
 });
 let submit = () => {
-    router.post("/users", form);
+    form.post("/users");
 };
 </script>
 
@@ -33,7 +28,7 @@ let submit = () => {
 
             <input
                 class="block w-full rounded-md px-4 py-2 border"
-                :class="errors.name ? 'border-red-500' : ''"
+                :class="form.errors.name ? 'border-red-500' : ''"
                 type="text"
                 autocomplete="name"
                 v-model="form.name"
@@ -42,9 +37,9 @@ let submit = () => {
                 required
             />
             <div
-                v-if="errors.name"
+                v-if="form.errors.name"
                 class="text-xs text-red-500 mt-1"
-                v-text="errors.name"
+                v-text="form.errors.name"
             />
         </div>
         <div class="px-8">
@@ -56,7 +51,7 @@ let submit = () => {
 
             <input
                 class="block w-full rounded-md px-4 py-2 border"
-                :class="errors.email ? 'border-red-500' : ''"
+                :class="form.errors.email ? 'border-red-500' : ''"
                 type="email"
                 autocomplete="username"
                 v-model="form.email"
@@ -65,9 +60,9 @@ let submit = () => {
                 required
             />
             <div
-                v-if="errors.email"
+                v-if="form.errors.email"
                 class="text-xs text-red-500 mt-1"
-                v-text="errors.email"
+                v-text="form.errors.email"
             />
         </div>
         <div class="px-8">
@@ -80,7 +75,7 @@ let submit = () => {
             <div class="relative">
                 <input
                     class="block w-full rounded-md px-4 py-2 border"
-                    :class="errors.password ? 'border-red-500' : ''"
+                    :class="form.errors.password ? 'border-red-500' : ''"
                     type="password"
                     autocomplete="new-password"
                     v-model="form.password"
@@ -90,15 +85,16 @@ let submit = () => {
                 />
             </div>
             <div
-                v-if="errors.password"
+                v-if="form.errors.password"
                 class="text-xs text-red-500 mt-1"
-                v-text="errors.password"
+                v-text="form.errors.password"
             />
         </div>
         <div class="px-8">
             <button
                 type="submit"
                 class="rounded-full bg-blue-500 px-10 py-2 text-sm font-bold uppercase text-white transition-colors duration-300 hover:bg-blue-600"
+                :disabled="form.processing"
             >
                 Create
             </button>
